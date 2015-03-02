258bd575b5e57a580a650bd02844bf7526ceee70
Hyunkyul <hklee634@gmail.com>
2015-03-02 18:57:57 +0900

    Monday done
    - class DnaData
    +- data members
    - refData
    - geneticMap
    + Pt(i)
    + aMatrix()
    + bMatrix()
    etc

---
+++ hmm.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hmm.c
+ *
+ *    Description:  Hidden Markov Model
+ *
+ *        Version:  1.0
+ *        Created:  03/02/2015 16:40:50
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME Hyunkyu Lee 
+ *   Organization:  Han Lab 
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+   
+
+
+
+
+
+
+
+
+
+
