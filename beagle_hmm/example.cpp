57b0400ad43b4f0d0050fe81fb1ea7b5127fe77e
Hyunkyul <hklee634@gmail.com>
2015-03-16 19:43:02 +0900

    monday

--- beagle_hmm/example.cpp
+++ beagle_hmm/example.cpp
@@ -22,9 +22,10 @@
 
 int main(int argc, char **argv)
 {
+
 	FILE *fin;
-	char *f1;
-	char *f2;
+	char *nodecountf;
+	char *edgepathf;
 
 	int i,j,h,k; //universal variables
 	int **nodecount; //node count
@@ -32,8 +33,8 @@ int main(int argc, char **argv)
 	int D; //number of levels
 
 	i=1;
-	f1= argv[i++];
-	f2= argv[i++];
+	nodecountf= argv[i++];
+	edgepathf= argv[i++];
 	D=4;
 
 	nodecount = (int**)malloc((D+1)*sizeof(int*));
@@ -46,46 +47,106 @@ int main(int argc, char **argv)
 
 // file read
 
-	fin = fopen(f1,"r");
+	fin = fopen(nodecountf,"r");
 	for (i=0;i<D+1;i++)
 	{
 		for (j=0;j<16; j++)
 		{
-		fscanf(fin, "%d", &nodecount[i][j]); //ibe at a tune to address at the nodecount size
+		fscanf(fin, "%d", &nodecount[i][j]); //be at a tune to address at the nodecount size
 		printf("%d ",nodecount[i][j]);
 		}
 		printf("\n");
 
    }
  
-	fin = fopen(f2,"r");
+	fin = fopen(edgepathf,"r");
  	for (i=0;i<D+1;i++)
 	{
 		for (j=0;j<16; j++)
 		{
-		fscanf(fin, "%d", &edgepath[i][j]); //ibe at a tune to address at the nodecount size
+		fscanf(fin, "%d", &edgepath[i][j]); //be at a time to address at the nodecount size
 		printf("%d ",edgepath[i][j]);
 		}
 	printf("\n");
 	}
 
-//file read end  
+//Beagle HMM!
+	
+	int *missing;
+	missing = (int*)malloc(D*sizeof(int));
 
-
-//file input
-
-//print out file 
-	for (i=0;i<5;i++)
+	printf("\nmissing i : ");
+	for (i=0;i<D;i++) 
 	{
-		for (j=0;j<16; j++)
+		if(i==0) missing[i]=1;
+		else missing[i]=0;
+		printf("%d ",missing[i]);
+	}
+	printf("\n");
+
+
+//forward algorithm  
+
+	double **alpha;
+	int **boolean;
+
+   boolean = (int**)malloc(D*sizeof(int*));
+	for (i=0;i<D;i++)
+		boolean[i] = (int*)malloc(pow(2,D)*sizeof(int));
+
+	alpha = (double**)malloc(D*sizeof(double*));
+	for (i=0;i<D;i++)
+		alpha[i] = (double*)malloc(pow(D,2)*sizeof(double));
+
+	printf("\nboolean : \n"); //build boolean to calculate P(ei)*P(ei)
+	for (i=0;i<D;i++)
+	{
+		for (j=0;j<pow(2,D);j++)
 		{
-		printf("%d ",nodecount[i][j]);
+			if(nodecount[i+1][j]!=0) boolean[i][j]=1;
+			else boolean[i][j]=0;
+			printf("%d",boolean[i][j]);
+		}
+		printf("\n");
+	}	
+
+	// calculate alpha
+	
+	for (i=0;i<D;i++)// for D*D states
+	{	
+		for(j=0;j<D;j++) alpha[0][D*i+j]= boolean[1][i]*boolean[1][j]*(nodecount[1][i]*nodecount[1][j]/pow(nodecount[0][0],2));
+	}
+
+	for (i=0;i<D;i++)
+	{
+		for (j=0;j<pow(D,2); j++)
+		{
+		printf("%.2f ",alpha[i][j]);
 		}
 	printf("\n");
 	}
 
-}
 
+
+	for (i=1;i<D;i++)// for D*D states
+	{	
+		for (j=0;j<D;j++)
+		{
+			for(k=0;k<d;k++)
+			{
+				if (boolean[i][j]*boolean[i][k]==1) //only if both booleans are 1 
+				{
+					for (h=0;h<pow(D,2);h++) 
+					{
+						if(edgepath[j][k]==edgepath[i][h/4]&&edgepath[j][k]==edgepath[i][h%4]) 
+							alpha[i][D*j+k]+=alpha[i-1][h]*nodecount[i][h%4]/nodecount[i-1][h/4 
+					}
+				}
+			}
+		}
+	}
+
+}
 
 
 
